2500655f85cedae0b750c5b8d95ad1b7f8887c2a
Patrick <patrick.krisko@gmail.com>
2018-04-29 15:08:25 -0400

    CLI Skeleton

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AutoComponent } from './auto/auto.component';
+import { AutoHomeComponent } from './auto/auto-home/auto-home.component';
+import { AutoAboutComponent } from './auto/auto-about/auto-about.component';
+import { AutoTeamComponent } from './auto/auto-team/auto-team.component';
+import { AutoBodyShopComponent } from './auto/auto-body-shop/auto-body-shop.component';
+import { AutoContactUsComponent } from './auto/auto-contact-us/auto-contact-us.component';
+import { AutoReviewsComponent } from './auto/auto-reviews/auto-reviews.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AutoComponent,
+    AutoHomeComponent,
+    AutoAboutComponent,
+    AutoTeamComponent,
+    AutoBodyShopComponent,
+    AutoContactUsComponent,
+    AutoReviewsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
